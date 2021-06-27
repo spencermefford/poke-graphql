e2c7969d868d2fa203c0689ed2fa42c5efc839b1
Spencer Mefford <smefford@nav.com>
2021-06-26 18:34:03 -0600

    Linting

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+import express from 'express'
+import { ApolloServer, gql } from 'apollo-server-express'
 
 export default async function startApolloServer() {
   // Construct a schema, using GraphQL schema language
@@ -7,23 +7,22 @@ export default async function startApolloServer() {
     type Query {
       hello: String
     }
-  `;
+  `
 
   // Provide resolver functions for your schema fields
   const resolvers = {
     Query: {
       hello: () => 'Hello world!',
     },
-  };
+  }
 
-  
-  const server = new ApolloServer({ typeDefs, resolvers });
-  await server.start();
+  const server = new ApolloServer({ typeDefs, resolvers })
+  await server.start()
 
-  const app = express();
-  server.applyMiddleware({ app });
+  const app = express()
+  server.applyMiddleware({ app })
 
-  await new Promise(resolve => app.listen({ port: 4000 }, resolve));
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
-  return { server, app };
+  await new Promise<void>((resolve) => app.listen({ port: 4000 }, resolve))
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  return { server, app }
 }
