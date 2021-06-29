be5e23f08765e38267d16c8c6213e716a3602073
Spencer Mefford <smefford@nav.com>
2021-06-29 11:40:56 -0600

    Added the rate limiter and the API keys

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
 import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer, AuthenticationError } from 'apollo-server-express';
+import rateLimit from 'express-rate-limit';
 import typeDefs from './graphql/schema';
 import resolvers from './graphql/resolvers';
 import { DataSources } from './types/graphql';
 import PokemonDataSource from './graphql/PokemonDataSource';
+
+// Normally these keys would live in a DB table, but this works for a simple example
+const PARTNERS = [
+  { id: 'partner-a', apiKey: '123-ABC' },
+  { id: 'partner-b', apiKey: '456-DEF' },
+];
+
+const getApiKey = (req: express.Request): string => req.get('X-Api-Key') ?? '';
 
 export default async function startApolloServer() {
   const server = new ApolloServer({
@@ -12,10 +21,27 @@ export default async function startApolloServer() {
     dataSources: (): DataSources => ({
       pokemonDataSource: new PokemonDataSource(),
     }),
+    context: ({ req }) => {
+      // API Key Validation
+      const partner = PARTNERS.find((p) => p.apiKey === getApiKey(req));
+      if (!partner)
+        throw new AuthenticationError(
+          'You do not have permission to access this resource.',
+        );
+    },
   });
   await server.start();
 
   const app = express();
+
+  // Setup Rate Limiting
+  const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minutes
+    max: 5, // Limit each IP to X requests per windowMs
+    keyGenerator: (req) => getApiKey(req), // Limit based on partner
+  });
+  app.use(limiter);
+
   server.applyMiddleware({ app });
 
   await new Promise<void>((resolve) => app.listen({ port: 4000 }, resolve));
